f4308fd65019a42050557ece0bfd04fcf75d29a9
Christine Gregotski <tuj39467@temple.edu>
2020-09-27 14:57:51 -0400

    The program allows the user to access the drop down view of the spinner to choose a color that it will change the background color to.

--- app/src/main/java/edu/temple/colorspinner/ColorActivity.java
+++ app/src/main/java/edu/temple/colorspinner/ColorActivity.java
@@ -1,14 +1,21 @@
 package edu.temple.colorspinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+
 
 public class ColorActivity extends AppCompatActivity {
 
     Spinner spinner;
+    ConstraintLayout myLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +23,53 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.spinner);
+        myLayout = findViewById(R.id.myLayout);
 
-        String[] colors = {"Choose a color","Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
+        final String[] colors = {"Choose a color", "Red", "Yellow", "Green", "Blue", "Cyan", "Black", "Magenta","Gray","Light gray","Dark gray"};
 
-        ArrayAdapter ColorAdapter = new ArrayAdapter(ColorActivity.this, android.R.layout.simple_list_item_1,colors);
-
+        ArrayAdapter<String> ColorAdapter = new ArrayAdapter<String>(ColorActivity.this, android.R.layout.simple_list_item_1, colors);
         spinner.setAdapter(ColorAdapter);
+        ColorAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(colors[position].equals("Red")) {
+                      myLayout.setBackgroundColor(Color.RED);
+                }
+                else if(colors[position].equals("Yellow")) {
+                    myLayout.setBackgroundColor(Color.YELLOW);
+                }else if(colors[position].equals("Green")) {
+                    myLayout.setBackgroundColor(Color.GREEN);
+                }else if(colors[position].equals("Blue")) {
+                    myLayout.setBackgroundColor(Color.BLUE);
+                }else if(colors[position].equals("Cyan")) {
+                    myLayout.setBackgroundColor(Color.CYAN);
+                }else if(colors[position].equals("Black")) {
+                    myLayout.setBackgroundColor(Color.BLACK);
+                }else if(colors[position].equals("Magenta")) {
+                    myLayout.setBackgroundColor(Color.MAGENTA);
+                }else if(colors[position].equals("Gray")) {
+                    myLayout.setBackgroundColor(Color.GRAY);
+                }else if(colors[position].equals("Light gray")) {
+                    myLayout.setBackgroundColor(Color.LTGRAY);
+                }else if(colors[position].equals("Dark gray")) {
+                    myLayout.setBackgroundColor(Color.DKGRAY);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+
+        });
     }
-
-
 }
-
 
 
 
